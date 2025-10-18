abb303e690d288249caac0759d55964c8d02c3ff
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2025-10-18 17:58:30 +0100

    First Commit

---
+++ src/test/java/tn/esprit/gestion_projet/GestionProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.gestion_projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProjetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
